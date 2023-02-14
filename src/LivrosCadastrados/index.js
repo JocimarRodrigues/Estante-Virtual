c138d9177f56029dfb30a5995d931688f5c65142
jocimar <jocimarrodrigues1337@gmail.com>
2023-02-14 09:19:58 -0300

    Modal Finalizado, começando a estilização

--- src/LivrosCadastrados/index.js
+++ src/LivrosCadastrados/index.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 import Estante from "../components/Estante";
 import "./LivrosCadastrados.css";
 import { useState } from "react";
@@ -6,31 +6,44 @@ import Modal from "../components/Modal";
 
 
 
+
+
+
+
 const LivrosCadastrados = ({titulo, imagem, genero, quantidade, preco, modal}) => {
   //console.log(props)
  // console.log(props.categorias)
+ const [mostrarModal, setMostrarModal] = useState(false);
 
-  const mostrarModal = () => {
-    let modal = document.getElementById('modala')
-    modal.style.display = 'flex'
-  }
+ const abrirModal = () => {
+     setMostrarModal(true);
+ }
+ 
+ const fecharModal = () => {
+  setMostrarModal(false)
+ }
+
+
+
+
   
-
   return (
     <section className='lista-livros'>
         <div key={titulo} className='card-livro'>
           <h1>{genero}</h1>
-          <img src={imagem} alt={imagem} style={{cursor: 'pointer'}} onClick={mostrarModal}></img>
+          <img src={imagem} alt={imagem} style={{cursor: 'pointer'}} onClick={abrirModal}></img>
           <h1>{titulo}</h1>
           <h1 id="h21">{quantidade}</h1>
           <h1>{preco}</h1>         
         </div>
-        <div className="modal" id='modala'>
-          <Modal 
-          titulo={titulo}
-          quantidade={quantidade}
-          preco={preco}/>
-        </div>
+        <div>
+        <Modal 
+        mostrarModal={mostrarModal} 
+        fecharModal={fecharModal}
+        imagem={imagem}
+        titulo={titulo}
+        />
+    </div>
     </section>
   );
 
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,14 +1,40 @@
 import './Modal.css'
+import React, { useState } from 'react'
 
-const Modal = ({titulo, quantidade, preco}) => {
-    return (
-        <div className='modal' id='modala'>
-            <h1>{titulo}</h1>
-            <h2>{quantidade}</h2>
-            <h3>{preco}</h3>
+const Modal = ({mostrarModal, titulo ,fecharModal, imagem}) => {
+
+
+
+  return (
+    <>
+
+    {mostrarModal && (
+
+    <div className='conteudo'
+    >
+        <div className='overlay'
+        >
+            
+        <div className='fechar'>
+        <button onClick={fecharModal}>X</button>
         </div>
-        
-    )
+        <img src={imagem}></img>
+        <h1>{titulo}</h1>
+        </div>
+    </div>
+    )}
+    </>
+  )
 }
 
 export default Modal;
+
+/* const [mostrarModal, setMostrarModal] = useState(false);
+
+const abrirModal = () => {
+    setMostrarModal(true);
+}
+
+const fecharModal = () => {
+    setMostrarModal(false);
+} */
